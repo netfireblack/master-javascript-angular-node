f699f5b1502d243146fcc64654a968ae08dd8582
netfireblack <pablo.metalheart@gmail.com>
2022-11-24 21:56:56 -0300

    sexto commit

---
+++ 65.objetos/js/main.js
@@ -0,0 +1,24 @@
+// 1° forma de declarar objetos mediante literal
+
+let jugador1 = {
+    nombre: 'Sergio',
+    apellidos: 'Ramos', // propiedades son públicas
+    dorsal: '4',
+    goles: 0,
+    equipos: ['Sevilla', 'Real Madrid'],
+    marcarGol() {
+        this.goles++;
+    }
+}
+
+// Acceso a los miembros o propiedades (notación del punto)
+
+console.log(jugador1.apellidos);
+
+jugador1.marcarGol();
+
+console.log(jugador1.goles);
+
+jugador1.apellidos = 'Ramos González';
+
+console.log(jugador1.apellidos);
